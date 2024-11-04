87895e3c953321cc705a73e164ccaa33ff9e5e91
Atul tiwari <162774395+Gundayrules@users.noreply.github.com>
2024-11-04 16:27:57 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -44,10 +44,10 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://te.legra.ph/file/e3c1ae96d329acd2d99cf.jpg"
+        "START_IMG", "https://ibb.co/P1ThVpF"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "BRANDED_WORLD")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "ladliupdate)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -141,7 +141,7 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(6258877205)
+DEV_USERS.add(6896771363)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
